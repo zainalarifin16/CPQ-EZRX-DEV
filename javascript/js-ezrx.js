9acabd43965c36b3d28b23eaec869d7ed2be3c60
Zainal Arifin <arifin.1602@gmail.com>
2018-03-31 01:43:59 +0700

    btn manage in commerce management

--- javascript/js-ezrx.js
+++ javascript/js-ezrx.js
@@ -3594,11 +3594,18 @@
         // manage
         /* custom search and refine list order. */
         $('.commerce-sidebar-item').each(function(i, sbitem) {
-            if ($(sbitem).text().toLowerCase().indexOf('manage') != -1) {
+
+            if ($(sbitem).attr("href").indexOf('displayViewManager') != -1) {
                 $('#jg-tool-manage').attr('href', $(sbitem).attr('href'));
             } else if ($(sbitem).text().toLowerCase().indexOf('refine') != -1) {
                 $('#jg-tool-refine').attr('href', $(sbitem).attr('href'));
             }
+
+            /* if ($(sbitem).text().toLowerCase().indexOf('manage') != -1) {
+                $('#jg-tool-manage').attr('href', $(sbitem).attr('href'));
+            } else if ($(sbitem).text().toLowerCase().indexOf('refine') != -1) {
+                $('#jg-tool-refine').attr('href', $(sbitem).attr('href'));
+            } */
         });
 
         // folders
